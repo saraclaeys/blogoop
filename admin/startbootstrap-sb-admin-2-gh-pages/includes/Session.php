67ecac440b3cd13e87f18a80269f74a76046bd44
saraclaeys <sara.claeys01@gmail.com>
2020-05-07 11:53:47 +0200

    login methode

--- admin/startbootstrap-sb-admin-2-gh-pages/includes/Session.php
+++ admin/startbootstrap-sb-admin-2-gh-pages/includes/Session.php
@@ -12,6 +12,19 @@ class Session
         $this->check_the_login();
     }
 
+    public function is_signed_in()
+    {
+        return $this->signed_in;
+    }
+
+    public function login($user)
+    {
+        if ($user) {
+            $this->user_id = $_SESSION['user_id'] = $user->id;
+            $this->signed_in = true;
+        }
+    }
+
     private function check_the_login()
     {
         if (isset($_SESSION['user_id'])) {
