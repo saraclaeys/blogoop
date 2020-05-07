cfe120f0e6a9ef0b09129c61c6a56c0a439bc6a9
saraclaeys <sara.claeys01@gmail.com>
2020-05-07 10:55:34 +0200

    foreach has_the_attribute

--- admin/startbootstrap-sb-admin-2-gh-pages/includes/User.php
+++ admin/startbootstrap-sb-admin-2-gh-pages/includes/User.php
@@ -9,33 +9,42 @@ class User
     public $first_name;
     public $last_name;
 
-    public static function instantie($result){
+    public static function instantie($result)
+    {
         $the_object = new self();
-        $the_object->id = $result['id'];
-        $the_object->username = $result['username'];
-        $the_object->password = $result['password'];
-        $the_object->first_name = $result['first_name'];
-        $the_object->last_name = $result['last_name'];
+        foreach ($result as $the_attribute => $value) {
+            if ($the_object->has_the_attribute($the_attribute)) {
+                $the_object->$the_attribute = $value;
+            }
+        }
         return $the_object;
     }
 
-    public static function  find_this_query($sql){
+    private function has_the_attribute($the_attribute){
+        $object_properties = get_object_vars($this);
+        return array_key_exists($the_attribute, $object_properties);
+    }
+
+    public static function find_this_query($sql)
+    {
         global $database;
         $result = $database->query($sql);
         $the_object_array = array();
-        while ($row = mysqli_fetch_array($result)){
+        while ($row = mysqli_fetch_array($result)) {
             $the_object_array[] = self::instantie($row);
         }
         return $the_object_array;
     }
 
-    public static function  find_all_users(){
+    public static function find_all_users()
+    {
         return self::find_this_query("SELECT * FROM user");
     }
 
-    public static function find_user_by_id($user_id){
+    public static function find_user_by_id($user_id)
+    {
         $result = self::find_this_query("SELECT * FROM user WHERE id = $user_id");
-        if (!empty($result)){
+        if (!empty($result)) {
             return array_shift($result);
         } else {
             return false;
