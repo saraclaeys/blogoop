397d389623a554f7ed92cd5e5aeff99c0dd38f80
saraclaeys <sara.claeys01@gmail.com>
2020-05-07 11:46:04 +0200

    set user_id in Session class

--- admin/startbootstrap-sb-admin-2-gh-pages/includes/Session.php
+++ admin/startbootstrap-sb-admin-2-gh-pages/includes/Session.php
@@ -3,13 +3,25 @@
 
 class Session
 {
-private $signed_in;
-public $user_id;
+    private $signed_in = false;
+    public $user_id;
 
-function __construct()
-{
-    session_start();
-}
+    function __construct()
+    {
+        session_start();
+        $this->check_the_login();
+    }
+
+    private function check_the_login()
+    {
+        if (isset($_SESSION['user_id'])) {
+            $this->user_id = $_SESSION['user_id'];
+            $this->signed_in = true;
+        } else {
+            unset($this->user_id);
+            $this->signed_in = false;
+        }
+    }
 }
 
 $session = new Session();
