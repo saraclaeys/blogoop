28d4353eabe3826c01165623017ed7475785ec56
saraclaeys <sara.claeys01@gmail.com>
2020-05-07 14:20:43 +0200

    login php

--- admin/startbootstrap-sb-admin-2-gh-pages/login.php
+++ admin/startbootstrap-sb-admin-2-gh-pages/login.php
@@ -1,24 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php require_once ("includes/header.php"); ?>
 
-<head>
+<?php
+if($session->is_signed_in()){
+    redirect("index.php");
+}
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+if (isset($_POST['submit'])){
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
 
-  <title>SB Admin 2 - Login</title>
+    $user_found = User::verify_user($username, $password);
 
-  <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-  <!-- Custom styles for this template-->
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
-</head>
+    if($user_found){
+        $session->login($user_found);
+        redirect("index.php");
+    } else {
+        $the_message = "Your password or username FAILED";
+    }
+} else {
+    $username = "";
+    $password = "";
+}
+?>
 
 <body class="bg-gradient-primary">
 
