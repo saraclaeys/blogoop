2a8f61dd0f5e578ba4b91b4e63e25f64e6fb049e
saraclaeys <sara.claeys01@gmail.com>
2020-05-07 11:28:34 +0200

    includes to require_once

--- admin/startbootstrap-sb-admin-2-gh-pages/includes/header.php
+++ admin/startbootstrap-sb-admin-2-gh-pages/includes/header.php
@@ -1,4 +1,4 @@
-<?php include ('init.php'); ?>
+<?php require_once ('init.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 
--- admin/startbootstrap-sb-admin-2-gh-pages/includes/init.php
+++ admin/startbootstrap-sb-admin-2-gh-pages/includes/init.php
@@ -1,6 +1,7 @@
 <?php
-include ('functions.php');
-include ('config.php');
-include ('Database.php');
-/*include ('User.php');*/
+require_once ('functions.php');
+require_once ('config.php');
+require_once ('Database.php');
+require_once ('User.php');
+require_once ('Session.php');
 ?>
