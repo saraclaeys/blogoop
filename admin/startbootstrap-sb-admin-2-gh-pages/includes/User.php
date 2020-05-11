d62d198a26ad05e5dc8abe67d57525b84f748f1f
saraclaeys <sara.claeys01@gmail.com>
2020-05-11 13:41:44 +0200

    function create()

--- admin/startbootstrap-sb-admin-2-gh-pages/includes/User.php
+++ admin/startbootstrap-sb-admin-2-gh-pages/includes/User.php
@@ -68,6 +68,22 @@ class User
     }
 
     public function create(){
+        global $database;
 
+        $sql = "INSERT INTO user (username, password, first_name, last_name)";
+        $sql .= " VALUES ";
+        $sql .= $database->escape_string($this->username) . "', '";
+        $sql .= $database->escape_string($this->password) . "', '";
+        $sql .= $database->escape_string($this->first_name) . "', '";
+        $sql .= $database->escape_string($this->last_name) . "', '";
+
+        if ($database->query($sql)){
+            $this->id = $database->the_insert_id();
+            return true;
+        } else {
+            return false;
+        }
+
+        $database->query($sql);
     }
 }
