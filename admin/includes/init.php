86a87aa4acbb9905bc17562d1a7fb491496ec7d4
saraclaeys <sara.claeys01@gmail.com>
2020-05-12 14:30:32 +0200

    init require_once(Db_object.php)

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -2,6 +2,7 @@
 require_once ("functions.php");
 require_once ("config.php");
 require_once ("Database.php");
+require_once ("Db_object.php");
 require_once ("User.php");
 require_once ("Session.php");
 ?>
