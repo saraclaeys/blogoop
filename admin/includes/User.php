c3cc99b4d999c9c941e321fefcdf00dd34504ab7
saraclaeys <sara.claeys01@gmail.com>
2020-05-12 09:33:41 +0200

    fix update()

--- admin/includes/User.php
+++ admin/includes/User.php
@@ -91,10 +91,10 @@ class User
         global $database;
 
         $sql = "UPDATE user SET ";
-        $sql .= "username '" . $database->escape_string($this->username) . "' , ";
-        $sql .= "password '" . $database->escape_string($this->password) . "' , ";
-        $sql .= "first_name '" . $database->escape_string($this->first_name) . "' , ";
-        $sql .= "last_name '" . $database->escape_string($this->last_name) . "' ";
+        $sql .= "username= '" . $database->escape_string($this->username) . "' , ";
+        $sql .= "password= '" . $database->escape_string($this->password) . "' , ";
+        $sql .= "first_name= '" . $database->escape_string($this->first_name) . "' , ";
+        $sql .= "last_name= '" . $database->escape_string($this->last_name) . "' ";
         $sql .= "WHERE id = " . $database->escape_string($this->id);
 
         $database->query($sql);
