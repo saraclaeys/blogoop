7c05601b8f042626cdea194b8f9a9536150d87a8
saraclaeys <sara.claeys01@gmail.com>
2020-05-12 15:38:28 +0200

    vars Photo class

---
+++ admin/includes/Photo.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class Photo extends Db_object
+{
+
+    protected static $db_table = "photo";
+    protected static $db_table_fields = array('title', 'description', 'filename', 'type', 'size');
+    public $photo_id;
+    public $title;
+    public $description;
+    public $filename;
+    public $type;
+    public $size;
+
+}
