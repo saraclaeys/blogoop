f4479f5a2dbe2adcc035b1d98c218f292ef49469
saraclaeys <sara.claeys01@gmail.com>
2020-05-12 16:22:14 +0200

    site root, includes path and images path in init.php

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,4 +1,10 @@
 <?php
+defined('DS')? null : define('DS', DIRECTORY_SEPARATOR);
+define('SITE_ROOT', DS . 'wamp64' . DS . 'www' . DS . 'blogoop');
+defined('INCLUDES_PATH') ? null : define('INCLUDES_PATH', SITE_ROOT . DS . 'admin' . DS . 'includes');
+defined('IMAGE_PATH') ? null : define('IMAGES_PATH', SITE_ROOT . DS . 'admin' . DS . 'img');
+
+
 require_once ("functions.php");
 require_once ("config.php");
 require_once ("Database.php");
