c33c666850fe40347dda486792a8c2b8a3fd0004
saraclaeys <sara.claeys01@gmail.com>
2020-05-19 09:53:58 +0200

    delete_photo.php

---
+++ admin/delete_photo.php
@@ -0,0 +1,29 @@
+<?php
+
+include('includes/header.php');
+
+if (!$session->is_signed_in()) {
+    redirect('login.php');
+}
+
+if (empty($_GET['id'])) {
+    redirect('photos.php');
+}
+
+$photo = Photo::find_by_id($_GET['id']);
+
+if ($photo) {
+    $photo->delete_photo();
+    redirect('photos.php');
+} else {
+    redirect('photos.php');
+}
+
+include('includes/sidebar.php');
+include('includes/content-top.php');
+
+?>
+
+<h1>Welkom op de delete foto pagina</h1>
+
+<?php include('includes/footer.php'); ?>
