6a9a8a1a23c44af976dbdcf02262c407538b9674
saraclaeys <sara.claeys01@gmail.com>
2020-05-21 09:24:51 +0200

    edit_user.php html

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -33,30 +33,31 @@ if (isset($_POST['submit'])) {
 <div class="container-fluid">
     <div class="row">
         <div class="col-12">
-            <h2>Welkom op de add user pagina</h2>
-            <form action="add_user.php" method="post">
+            <h2>Welkom op de edit user pagina</h2>
+            <form action="" method="post">
                 <div class="col-md-8">
                     <div class="form-group">
                         <label for="title">Username:</label>
-                        <input type="text" name="username" class="form-control" value="">
+                        <input type="text" name="username" class="form-control" value=" <?php echo $user->username; ?>">
                     </div>
                     <div class="form-group">
                         <label for="first_name">First name:</label>
-                        <input type="text" name="first_name" class="form-control" value="">
+                        <input type="text" name="first_name" class="form-control" value="<?php echo $user->first_name; ?>">
                     </div>
                     <div class="form-group">
                         <label for="last_name">Last name:</label>
-                        <input type="text" name="last_name" class="form-control" value="">
+                        <input type="text" name="last_name" class="form-control" value="<?php echo $user->last_name; ?>">
                     </div>
                     <div class="form-group">
                         <label for="password">Password:</label>
-                        <input type="password" name="password" class="form-control" value="">
+                        <input type="password" name="password" class="form-control" value="<?php echo $user->password; ?>">
                     </div>
                     <div class="form-group">
+                        <img src="<?php echo $user->image_path_and_placeholder(); ?>" alt="" class="img-fluid" width="40" height="40">
                         <label for="file">User image:</label>
-                        <input type="file" name="file" class="form-control" value="">
+                        <input type="file" name="file" class="form-control">
                     </div>
-                    <input type="submit" name="submit" value="Add user" class="btn btn-primary">
+                    <input type="submit" name="submit" value="Update user" class="btn btn-primary">
                 </div>
             </form>
         </div>
