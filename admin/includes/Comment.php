ada00b38534ee846a05ebf12fc4ec08894e43725
saraclaeys <sara.claeys01@gmail.com>
2020-05-21 18:53:09 +0200

    find_the_comment

--- admin/includes/Comment.php
+++ admin/includes/Comment.php
@@ -23,6 +23,15 @@ class Comment extends Db_object
         }
     }
 
+    public  static function find_the_comment($photo_id) {
+        global $database;
+        $sql = "SELECT * FROM " . self::$db_table;
+        $sql .= " WHERE photo_id = " . $database->escape_string($photo_id);
+        $sql .= " ORDER BY photo_id ASC";
+
+        return self::find_this_query($sql);
+    }
+
 
 }
 
