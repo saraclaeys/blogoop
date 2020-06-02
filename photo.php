1edc322cd1b13ddcd9f422424f54c3770feccb71
saraclaeys <sara.claeys01@gmail.com>
2020-06-02 14:09:41 +0200

    front end fixes

--- photo.php
+++ photo.php
@@ -1,165 +1,163 @@
 <?php
-include ('includes/header.php');
-// require_once ('admin/includes/init.php');
+include ("includes/header.php");
+require_once ("admin/includes/init.php");
 
-if (empty($_GET['id'])){
+if(empty($_GET['id'])){
     redirect('index.php');
 }
 
 $photo = Photo::find_by_id($_GET['id']);
-// echo $photo->title;
+//var_dump($photo);
 
-if (isset($_POST['submit'])){
-   // echo 'hello';
+if(isset($_POST['submit'])){
     $author = trim($_POST['author']);
     $body = trim($_POST['body']);
 
-    $new_comment = Comment::create_comment($photo->id, $author, $body);
+    $new_comment = Comment::create_comment($photo->id,$author,$body);
 
-    if ($new_comment && $new_comment->save()){
+    if($new_comment && $new_comment->save()){
         redirect("photo.php?id={$photo->id}");
-    } else {
-        $message = "There are some problems saving";
+    }else{
+        $message = "There are some saving problems";
     }
-} else {
+}else{
     $author = "";
     $body = "";
 }
 
 $comments = Comment::find_the_comment($photo->id);
-
+//var_dump($comments);
 ?>
 
-<!-- Page Content -->
-<div class="container">
+    <!-- Page Content -->
+    <div class="container">
 
-    <div class="row">
+        <div class="row">
 
-        <!-- Post Content Column -->
-        <div class="col-lg-8">
+            <!-- Post Content Column -->
+            <div class="col-lg-8">
 
-            <!-- Title -->
-            <h1 class="mt-4"> <?php echo $photo->title; ?> </h1>
+                <!-- Title -->
+                <h1 class="mt-4"><?php echo $photo->title;  ?></h1>
 
-            <!-- Author -->
-            <p class="lead">
-                by
-                <a href="#">Start Bootstrap</a>
-            </p>
+                <!-- Author -->
+                <p class="lead">
+                    by
+                    <a href="#">Start Bootstrap</a>
+                </p>
 
-            <hr>
+                <hr>
 
-            <!-- Date/Time -->
-            <p>Posted on January 1, 2019 at 12:00 PM</p>
+                <!-- Date/Time -->
+                <p>Posted on January 1, 2019 at 12:00 PM</p>
 
-            <hr>
+                <hr>
 
-            <!-- Preview Image -->
-            <img class="img-fluid rounded" src="<?php echo 'admin' . DS . $photo->picture_path(); ?>" width="300" height="300" alt="">
+                <!-- Preview Image -->
+                <img class="img-fluid rounded" src="<?php echo 'admin'.DS.$photo->picture_path(); ?>" alt="<?php echo $photo->alternate_text; ?>">
 
-            <hr>
+                <hr>
 
-            <!-- Post Content -->
-            <p>
-                <?php echo $photo->description; ?>
-            </p>
-            <hr>
+                <!-- Post Content -->
+                <p>
+                    <?php echo $photo->description; ?>
+                </p>
+                <hr>
 
-            <!-- Comments Form -->
-            <div class="card my-4">
-                <h5 class="card-header">Leave a Comment:</h5>
-                <div class="card-body">
-                    <form method="post">
-                        <div class="form-group">
-                            <label for="author">Author</label>
-                            <input type="text" name="author" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <textarea class="form-control" name="body" rows="3"></textarea>
-                        </div>
-                        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-                    </form>
+                <!-- Comments Form -->
+                <div class="card my-4">
+                    <h5 class="card-header">Leave a Comment:</h5>
+                    <div class="card-body">
+                        <form method="post">
+                            <div class="form-group">
+                                <label for="author">Author:</label>
+                                <input type="text" name="author" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control" rows="3" name="body"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+                        </form>
+                    </div>
                 </div>
+
+                <?php foreach($comments as $comment): ?>
+                    <!-- Single Comment -->
+                    <div class="media mb-4">
+                        <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+                        <div class="media-body">
+                            <h5 class="mt-0"><?php echo $comment->author; ?> on photo <?php echo $comment->photo_id; ?></h5>
+                            <p><?php echo $comment->body; ?></p>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
             </div>
 
-            <?php foreach ($comments as $comment): ?>
+            <!-- Sidebar Widgets Column -->
+            <div class="col-md-4">
 
-            <!-- Single Comment -->
-            <div class="media mb-4">
-                <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
-                <div class="media-body">
-                    <h5 class="mt-0"><?php echo $comment->author; ?> on photo <?php $comment->photo_id; ?>></h5>
-                 <p> <?php echo $comment->body; ?> </p>
-                </div>
-            </div>
-
-            <?php endforeach; ?>
-
-        <!-- Sidebar Widgets Column -->
-        <div class="col-md-4">
-
-            <!-- Search Widget -->
-            <div class="card my-4">
-                <h5 class="card-header">Search</h5>
-                <div class="card-body">
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Search for...">
-                        <span class="input-group-btn">
+                <!-- Search Widget -->
+                <div class="card my-4">
+                    <h5 class="card-header">Search</h5>
+                    <div class="card-body">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search for...">
+                            <span class="input-group-btn">
                 <button class="btn btn-secondary" type="button">Go!</button>
               </span>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Categories Widget -->
-            <div class="card my-4">
-                <h5 class="card-header">Categories</h5>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled mb-0">
-                                <li>
-                                    <a href="#">Web Design</a>
-                                </li>
-                                <li>
-                                    <a href="#">HTML</a>
-                                </li>
-                                <li>
-                                    <a href="#">Freebies</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled mb-0">
-                                <li>
-                                    <a href="#">JavaScript</a>
-                                </li>
-                                <li>
-                                    <a href="#">CSS</a>
-                                </li>
-                                <li>
-                                    <a href="#">Tutorials</a>
-                                </li>
-                            </ul>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <!-- Side Widget -->
-            <div class="card my-4">
-                <h5 class="card-header">Side Widget</h5>
-                <div class="card-body">
-                    You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
+                <!-- Categories Widget -->
+                <div class="card my-4">
+                    <h5 class="card-header">Categories</h5>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <ul class="list-unstyled mb-0">
+                                    <li>
+                                        <a href="#">Web Design</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">HTML</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Freebies</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col-lg-6">
+                                <ul class="list-unstyled mb-0">
+                                    <li>
+                                        <a href="#">JavaScript</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">CSS</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Tutorials</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
+                <!-- Side Widget -->
+                <div class="card my-4">
+                    <h5 class="card-header">Side Widget</h5>
+                    <div class="card-body">
+                        You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
+                    </div>
+                </div>
+
             </div>
 
         </div>
+        <!-- /.row -->
 
     </div>
-    <!-- /.row -->
+    <!-- /.container -->
 
-</div>
-<!-- /.container -->
-
-<?php include ('includes/footer.php'); ?>
+<?php include("includes/footer.php"); ?>
