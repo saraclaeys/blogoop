df50ccf3241f0fbd6f32b34f757f18a2abd5ed1b
saraclaeys <sara.claeys01@gmail.com>
2020-06-05 13:36:13 +0200

    paginate line

--- index.php
+++ index.php
@@ -14,49 +14,60 @@ $sql .= "OFFSET {$paginate->offset()}";
 $photos = Photo::find_this_query($sql);
 ?>
 
-<!-- Page Content -->
-<div class="container-fluid">
+    <!-- Page Content -->
+    <div class="container-fluid">
 
-    <div class="row">
+        <div class="container">
 
-        <!-- Post Content Column -->
-        <div class="col-12">
+                <div class="row">
 
-            <!-- Title -->
-            <h1>Homepagina: photos</h1>
-            <?php foreach ($photos as $photo): ?>
-                <div class="col-3">
-                    <a href="photo.php?id= <?php echo $photo->id; ?> ">
-                        <img src="<?php echo 'admin' . DS . $photo->picture_path(); ?>" alt="<?php echo $photo->alternate_text; ?>" class="img-fluid">
-                    </a>
+                    <!-- Post Content Column -->
+                    <div class="col-12">
+
+                        <!-- Title -->
+                        <h1>Homepagina: photos</h1>
+                        <?php foreach ($photos as $photo): ?>
+                            <div class="col-3">
+                                <a href="photo.php?id= <?php echo $photo->id; ?> ">
+                                    <img src="<?php echo 'admin' . DS . $photo->picture_path(); ?>"
+                                         alt="<?php echo $photo->alternate_text; ?>" class="img-fluid">
+                                </a>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+
                 </div>
-            <?php endforeach; ?>
-        </div>
 
-        <div class="row">
-            <ul class="pager">
-                <?php
-                    if ($paginate->page_total() > 1){
-                        if ($paginate->has_next()){
-                            echo "<li class='next'><a href='index.php?page={$paginate->next()}'>Next</a></li>";
-                        }
-                        for ($i = 1; $i <= $paginate->page_total(); $i++) {
-                            if ($i == $paginate->current_page) {
-                                echo "<li class='active'><a href='index.php?page={$i}' > {$i} </a></li>";
-                            } else {
-                                echo "<li><a href='index.php?page={$i}' > {$i} </a> </li>";
+
+            <div class="container">
+                <div class="row">
+
+                    <div class="row">
+                        <ul class="pagination">
+                            <?php
+                            if ($paginate->page_total() > 1) {
+                                if ($paginate->has_next()) {
+                                    echo "<li class='next'><a href='index.php?page={$paginate->next()}'>Next</a></li>";
+                                }
+                                for ($i = 1; $i <= $paginate->page_total(); $i++) {
+                                    if ($i == $paginate->current_page) {
+                                        echo "<li class='active'><a href='index.php?page={$i}' > {$i} </a></li>";
+                                    } else {
+                                        echo "<li><a href='index.php?page={$i}' > {$i} </a> </li>";
+                                    }
+                                }
+                                if ($paginate->has_previous()) {
+                                    echo "<li class='previous'><a href='index.php?page={$paginate->previous()}'>Previous</a></li>";
+                                }
                             }
-                        }
-                        if ($paginate->has_previous()){
-                            echo "<li class='previous'><a href='index.php?page={$paginate->previous()}'>Previous</a></li>";
-                        }
-                    }
 
-                    ?>
-            </ul>
+                            ?>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
         </div>
-
     </div>
-</div>
 
-<?php include ('includes/footer.php'); ?>
+<?php include('includes/footer.php'); ?>
